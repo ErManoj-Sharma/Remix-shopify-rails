997402d6f532bcfef3e15911a3a245b9f044d6cf
manoj <mmmanojsharma201611@gmail.com>
2023-12-04 15:43:17 +0530

    Added product/update webhook Update Readme Doc

--- web/config/initializers/shopify_app.rb
+++ web/config/initializers/shopify_app.rb
@@ -4,6 +4,7 @@ ShopifyApp.configure do |config|
   config.webhooks = [
     # After a store owner uninstalls your app, Shopify invokes the APP_UNINSTALLED webhook
     # to let your app know.
+    { topic: "products/update", path: "api/webhooks/products_update" },
     { topic: "app/uninstalled", address: "api/webhooks/app_uninstalled" },
   ]
   config.application_name = "My Shopify App"
